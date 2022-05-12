ffbae966c3232afa2aca28c675042cefd26aee40
GrimStar <44913392+GrimStar@users.noreply.github.com>
2022-05-12 14:07:56 +1200

    Fixing code standards

--- AI_P01/AI_P01/WaveManager.cpp
+++ AI_P01/AI_P01/WaveManager.cpp
@@ -23,8 +23,8 @@ void WaveManager::AddSpawn(Spawn* _spawn)
 void WaveManager::Update(float _deltatime)
 {
 	//Updates the wave display
-	scene->getUI()->setCurrentWave(currentWave);
-	scene->getUI()->setNumberOfEnemies(currentEnemyCount);
+	scene->GetUI()->setCurrentWave(currentWave);
+	scene->GetUI()->setNumberOfEnemies(currentEnemyCount);
 
 	if (waveSpawnedEnemies >= totalWaveEnemies)
 	{
@@ -38,7 +38,7 @@ void WaveManager::Update(float _deltatime)
 
 	if (waveBreakTimer > 0.0f)
 	{
-		scene->getUI()->setWaveTimer(waveBreakTimer);
+		scene->GetUI()->setWaveTimer(waveBreakTimer);
 		waveBreakTimer -= _deltatime;
 	}
 	else
