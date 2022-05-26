0982b8b868953656b14d661ded30ad7e98835a38
WoofWoofGrr <76901281+WoofWoofGrr@users.noreply.github.com>
2022-05-26 22:59:15 +1200

    Another header

--- AI_P01/AI_P01/BackButton.h
+++ AI_P01/AI_P01/BackButton.h
@@ -1,3 +1,15 @@
+/***********************************************************************
+Bachelor of Software Engineering
+Media Design School
+Auckland
+New Zealand
+(c) 2022 Media Design School
+File Name : BackButton.h
+Description : A button to go back
+Author : Devon Millar, James Charnley, Sam Chandler, Mahalon
+Mail : devon.millar@mds.ac.nz, james.charnley@mds.ac.nz, samuel.chandler@mds.ac.nz, mahalon.Devoe@mds.ac.nz
+**************************************************************************/
+
 #pragma once
 #include "Button.h"
 class BackButton :
